68b60ba1e9b2f75eefe751e90b72f8c2dec9bbe3
Marianna <mckd2018@gmail.com>
2018-09-20 23:30:37 -0700

    add weapon and team classes as well as concept reference code

--- dog.py
+++ dog.py
@@ -1,22 +1,43 @@
-class Dog:
-    greeting = "Woof!"
+# Reference for learning classes and inhertiance
+# class Dog:
+#     greeting = "Woof!"
+#
+#     def __init__(self, name):
+#         self.name = name
+#
+#     def bark(self):
+#         print(self.greeting)
 
-    def __init__(self, name):
-        self.name = name
-
-    def bark(self):
-        print(self.greeting)
-
-my_first_dog = Dog("Annie")
-my_second_dog = Dog("Wyatt")
-
-print(my_first_dog.name)
-print(my_second_dog.name)
-
-my_first_dog.bark()
-my_second_dog.bark()
+# my_first_dog = Dog("Annie")
+# my_second_dog = Dog("Wyatt")
+#
+# print(my_first_dog.name)
+# print(my_second_dog.name)
+#
+# my_first_dog.bark()
+# my_second_dog.bark()
 
 # This always goes at the bottom
-if __name__ == "__main__":
-    my_dog = Dog()
-    my_dog.bark()
+# if __name__ == "__main__":
+#     my_dog = Dog()
+#     my_dog.bark()
+# ==============================================================================
+# Inheritance
+class Animal:
+    def __init__(self, name, sleep_duration): # There is an error with the argument and property not being the same
+        self.name = name
+        self.sleep_duration = sleep_duration
+
+    def sleep(self):
+        print(
+            "{} sleeps for {} hours.".format(
+            self.name,
+            self.sleep_duration))
+
+class Dog(Animal):
+    def bark(self):
+        print("Woof! Woof!")
+
+my_dog = Dog("Sophie", 12)
+my_dog.sleep()
+my_dog.bark()
--- superheroes.py
+++ superheroes.py
@@ -1,29 +1,6 @@
 import random
 
 
-class Ability:
-    def __init__(self, name, attack_strength):
-        #Initialize starting values
-        #Set Ability name
-        self.name = name
-        #Set attack strength
-        self.attack_strength = attack_strength
-
-    def attack(self):
-        # Return attack values
-        # Calculate lowest attack value as an integer.
-        lowest_attack_value = 20 // 2
-        # Use random.randint(a, b) to select a random attack value.
-        attack_value = random.ranint(2, 20)
-        # Return attack value between 0 and the full attack.
-        return attack_value
-
-    def update_attack(self, attack_strength):
-        # Update the value of the current attack strength with the # new value passed in as a parameter.
-        self = attack_strength
-
-
-
 class Hero:
     def __init__(self, name):
         self.abilities = list()
@@ -38,27 +15,85 @@ class Hero:
         # Add up and return the total of all attack_strength
         totalOfAttacks = 0
         for ability in self.abilities:
-            num = ability.attack_strength
             # attack(self)
-            totalOfAttacks += num
+            totalOfAttacks += ability.attack()
             # name of variable you assigned in the loop
             return totalOfAttacks
+
+
+class Ability:
+    def __init__(self, name, attack_strength):
+        #Initialize starting values
+        #Set Ability name
+        self.name = name
+        #Set attack strength
+        self.attack_strength = attack_strength
+
+    def attack(self):
+        # Return attack values
+        # Calculate lowest attack value as an integer.
+        # lowest_attack_value = self.attack_strength // 2
+        # Use random.randint(a, b) to select a random attack value.
+        attack_value = random.randint((self.attack_strength // 2), self.attack_strength)
+        # Return attack value between 0 and the full attack.
+        return attack_value
+
+    def update_attack(self, attack_strength):
+        # Update the value of the current attack strength with the # new value passed in as a parameter.
+        self.attack_strength = attack_strength
+
+
+class Weapon(Ability):
+    def attack(self):
+        # Typo in comments for this function
+        """
+        This method should return a random value between 0 and the full attack power of the weapon. Hint: The attack power is inherited.
+        """
+        weapon_attack_value = random.randint(0, self.attack_strength)
+        return weapon_attack_value
+
+
+class Team:
+    def init(self, team_name):
+        """Instantiate resources."""
+        self.name = team_name
+        self.heroes = list()
+
+    def add_hero(self, Hero):
+        """Add Hero object to heroes list."""
+        self.heroes.append(Hero)
+
+    def remove_hero(self, name):
+        """
+        Remove hero from heroes list.
+        If Hero isn't found return 0.
+        """
+        self.heroes.remove(self.Hero.name)
+        if Hero == ValueError:
+            return 0
+
+    def view_all_heroes(self):
+        """Print out all heroes to the console."""
+        for h in self.heroes:
+            print(hero)
 
 
 # Testing Code
 if __name__ == "__main__":
     hero = Hero("Wonder Woman")
     print("You have created a hero.".format(hero.name))
-    print("Number of Attacks: {}".format(hero.attack()))
+    print(hero.attack())
     # Create an ability
     ability = Ability("Divine Speed", 300)
     # Add the ability to the hero
     hero.add_ability(ability)
-    print("You have created an new ability (check properties of a ability).")
-    print("Number of Attacks: {}".format(hero.attack()))
+    # print("You have created an new ability (check properties of a ability).")
+    # print("Number of Attacks: {}".format(hero.attack()))
+    print(hero.attack())
     new_ability = Ability("Super Human Strength", 800)
-    print("You have created another ability.")
+    # print("You have created another ability.")
     hero.add_ability(new_ability)
-    print("You have added your ability to your hero.")
-    print("Now that you have added another ability the attack strength should be larger than before.")
-    print("Number of Attacks: {}".format(hero.attack()))
+    #print("You have added your ability to your hero.")
+    print(hero.attack())
+    # print("Now that you have added another ability the attack strength should be larger than before.")
+    # print("Number of Attacks: {}".format(hero.attack()))
